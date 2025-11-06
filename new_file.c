6ffe5f1c299e1e6486390817308f4e8153308989
dengchenxin <547054140@qq.com>
2025-11-06 10:11:40 +0800

    feat：新增函数

---
+++ new_file.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// 判断是否为质数
+static int is_prime(int n) //test
+{ 
++    // test command
+    if (n <= 1) return 0;
+#ifdef DEBUG
+    printf("Checking primality for: %d\n", n);
+#endif
+    if (n <= 3) 
+        return 1;
+    if (n % 2 == 0 || n % 3 == 0) return 0;
+
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
