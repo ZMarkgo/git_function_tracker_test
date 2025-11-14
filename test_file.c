a569ee598424d39ca72452d60cf452d2e973ff20
dengchenxin <547054140@qq.com>
2025-11-14 13:49:49 +0800

    test：新增条件编译测试函数

--- test_file.c
+++ test_file.c
@@ -18,3 +18,14 @@ int conditional_function(int x) {
     return x - 10;
 }
 #endif
+
+//构造一个带有if else条件编译的函数
+#ifdef MACRO_TEST
+int another_conditional_function(int x) {
+    return x * 2;
+}
+#else
+int another_conditional_function(int x) {
+    return x / 2;
+}
+#endif
