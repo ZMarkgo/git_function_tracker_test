4f4bdc92dc2d00625d7c2de572a87546fcca4212
dengchenxin <547054140@qq.com>
2025-11-17 15:46:13 +0800

    feat：新增测试函数

--- utils/math_utils3.c
+++ utils/math_utils3.c
@@ -7,3 +7,11 @@ int subtract(int a, int b) {
 int add(int a, int b) {
     return a + b;
 }
+
+int divide(int a, int b) {
+    if (b == 0) {
+        printf("Error: Division by zero\n");
+        return 0; // 返回0表示错误
+    }
+    return a / b;
+}
