20904315964b32461563181d501b133f4702407c
dengchenxin <547054140@qq.com>
2025-11-19 14:52:13 +0800

    feat：新增测试函数

--- utils1/math_utils.c
+++ utils1/math_utils.c
@@ -9,3 +9,11 @@ int subtract(int a, int b) {
     printf("Function A under MACRO is called.\n");
 }
 #endif
+
+int divide(int a, int b) {
+    if (b == 0) {
+        printf("Error: Division by zero.\n");
+        return 0; // 返回0表示错误
+    }
+    return a / b;
+}
