e12d5eb0c4f7019f24a5f671c3606d9d803aeb5e
dengchenxin <547054140@qq.com>
2025-11-28 16:19:15 +0800

    test：原本函数没有条件编译，后来增加了条件编译并新增了一个分支,该分支中新增了一个name相同但没有克隆的标识符实现。

--- new_file.c
+++ new_file.c
@@ -74,12 +74,18 @@ void function_A() {
 }
 #endif
 
+#ifdef M
 void funcA(){
     int a = 10;
     int b = 20;
     int sum = a + b;
     printf("Sum: %d\n", sum);
 }
+#else
+void funcA(){
+    
+}
+#endif
 
 
 
