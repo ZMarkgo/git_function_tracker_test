437bdf945618093684531d842c46223be3215141
dengchenxin <547054140@qq.com>
2025-12-01 10:33:49 +0800

    feat：新增测试函数

--- new_file3.c
+++ new_file3.c
@@ -22,3 +22,23 @@ int new_function(int x){
     return x*x;
 }
 
+#ifdef M
+void funcA(){
+    int a = 5;
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        sum += i;
+    }
+    printf("Macro M is defined. Sum = %d\n", sum);
+}
+#else
+void funcA(){
+    int b = 3;
+    int product = 1;
+    for(int i = 1; i <= b; i++){
+        product *= i;
+    }
+    printf("Macro M is not defined. Product = %d\n", product);
+}
+#endif
+
